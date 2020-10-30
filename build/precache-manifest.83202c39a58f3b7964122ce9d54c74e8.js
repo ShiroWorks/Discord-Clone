955ed3f1e4b0eabdabac0e6e1c66afe95b30ad6f
Szabo Vilmos <vili8899s@gmail.com>
2020-10-30 18:26:43 +0100

    init

---
+++ build/precache-manifest.83202c39a58f3b7964122ce9d54c74e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466dd58d6ae24e25c483b350c28db899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bf4d88c4ccbe039f6bc",
+    "url": "/static/css/main.1844fb2c.chunk.css"
+  },
+  {
+    "revision": "72dd99c32d59d554e407",
+    "url": "/static/js/2.5173bc40.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.5173bc40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf4d88c4ccbe039f6bc",
+    "url": "/static/js/main.bb28aa5d.chunk.js"
+  },
+  {
+    "revision": "fa430200eae998bbeca5",
+    "url": "/static/js/runtime-main.ee4f1273.js"
+  }
+]);
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+ //Your firebase config goes here
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
